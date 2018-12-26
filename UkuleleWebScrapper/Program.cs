67916201f79312e9dc978e365b4a981db97bc992
Leslie <mercier.les@gmail.com>
2018-12-26 17:46:46 -0500

    Changes to grab uke info from website and store it in a csv file. Added change to security protocol and load html from uri

--- UkuleleWebScrapper/Program.cs
+++ UkuleleWebScrapper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,44 +19,66 @@ namespace UkuleleWebScrapper
 
 		private static void loadPage()
 		{
+			string path = ".../.../Data/UkuleleData_"+ DateTime.Now.ToString("MMDDYYYYhhmmss") +".csv";
+			Directory.CreateDirectory(Path.GetDirectoryName(path));
+			System.IO.StreamWriter sw = new System.IO.StreamWriter(path);
 
 			//Allow Https sites to be loaded into html agility pack
+			WebClient client = new WebClient();
 			HtmlWeb web = new HtmlWeb()
 			{
 				PreRequest = request =>
 				{
 					// Make any changes to the request object that will be used.
 					request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+					ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
 					return true;
 				}
 			};
 
 
 			//Scrape Page for ukulele data
-			int pageCount = 1;
+			int pageCount = 1; //Total items divided by items per page is the page count
 			int pageCounter = 1;
+			double totalItems = 0;
+			int itemsPerPage = 90;
 			do
 			{
-				HtmlDocument document = web.Load("https://www.amazon.com/s/ref=sr_pg_" + pageCounter.ToString() +"?fst=as%3Aon&rh=n%3A11091801%2Cn%3A14733114011%2Cn%3A11971501%2Ck%3Aukulele&page="+ pageCounter.ToString()+"&sort=relevancerank&keywords=ukulele&ie=UTF8&qid=1545711444");
-				if(pageCounter == 1)
+				//string url = "https://www.guitarcenter.com/Ukuleles.gc#pageName=subcategory-page&N=19556&Nao=90&recsPerPage=90&postalCode=04957&radius=100&profileCountryCode=US&profileCurrencyCode=USD";
+				string formattedURL = "https://www.guitarcenter.com/Ukuleles.gc#pageName=subcategory-page&N=19556&Nao=" + (itemsPerPage * (pageCounter - 1)).ToString() + "&recsPerPage=" + itemsPerPage.ToString() + "&postalCode=04957&radius=100&profileCountryCode=US&profileCurrencyCode=USD";
+				Uri myUri = new Uri(formattedURL, UriKind.Absolute);
+				
+				//HtmlDocument document = web.Load(doc);
+
+				HtmlDocument document = new HtmlDocument();
+				document.LoadHtml(client.DownloadString(myUri));
+				Console.WriteLine("Starting Scrapper");
+				if (pageCounter == 1)
 				{
-					HtmlNodeCollection pageCountNode = document.DocumentNode.SelectNodes("//*[@id=\"pagn\"]//*[@class=\"pagnDisabled\"]");
-					if(!int.TryParse(pageCountNode[0].InnerText, out pageCount)) throw new Exception("Unable to parse page count into int");
+					string strTotalItems = document.DocumentNode.SelectSingleNode("//*[@class=\"searchTotalResults\"]/text()").InnerText;
+					if (!double.TryParse(strTotalItems, out totalItems)) throw new Exception("Unable to parse page count into int");
+					pageCount = (int)Math.Ceiling((totalItems / itemsPerPage));
 				}
-				HtmlNodeCollection ukeNodeUL = document.DocumentNode.SelectNodes("//*[@id=\"s-results-list-atf\"]");
-				int ulListItemCount = ukeNodeUL[0].ChildNodes.Count;
-				for(int li = 0; li < ulListItemCount; li++)
+				HtmlNodeCollection resultsContentListItems = document.DocumentNode.SelectNodes("//*[@id=\"resultsContent\"]/div/ol/li");
+				int listItemCount = resultsContentListItems.Count;
+				for(int li = 0; li < listItemCount; li++)
 				{
-					string result = ukeNodeUL[0].ChildNodes[li].GetAttributeValue("id", "");
-					HtmlNodeCollection ukeNode = document.DocumentNode.SelectNodes("//*[@id=\""+result+"\"]");
-					String ukeID = ukeNode[0].GetAttributeValue("data-asin", "noDataKey");
-					Console.WriteLine(ukeID);
+					//Save data to csv file
+					string ukeID = resultsContentListItems[li].SelectSingleNode("descendant::var[contains(@class, 'hidden productId')]/text()[normalize-space()]").InnerText.Replace("\n", "");
+					string ukeDesc = resultsContentListItems[li].SelectSingleNode("descendant::div[contains(@class, 'productTitle')]/a/text()[normalize-space()]").InnerText.Replace("\n", "");
+					string ukePrice = resultsContentListItems[li].SelectSingleNode("descendant::div[contains(@class, 'priceContainer mainPrice')]/span/text()[normalize-space()]").InnerText.Replace("\n", "");
+					Console.WriteLine("DataScrape: " + ukeID + "," + ukeDesc + "," + ukePrice);
+					sw.WriteLine(ukeID + "," + ukeDesc + "," + ukePrice);
+					
 				}
 				pageCounter++;
 
 			} while (pageCounter <= pageCount);
 
-			Console.ReadKey();
+			Console.WriteLine("Closing Scrapper");
+			sw.Close();
+			
 		}
+
 	}
 }
